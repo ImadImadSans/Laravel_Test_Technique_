a4ca51d7be05f6759314666ee9a30220b5ea8028
ImadImadSans <imad.elhassouni.30gmail.com>
2025-06-01 09:05:57 +0200

    Installation filament,création panneau administratif, modification PageAccueil

--- resources/views/PageAccueil.blade.php
+++ resources/views/PageAccueil.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="p-6">
-        <h1 class="text-xl font-bold mb-4">Bienvenue !</h1>
+        <h1 class="text-secondary font-bold mb-4">Bienvenue !</h1>
         <x-button>Réserver</x-button>
     </div>
 @endsection
